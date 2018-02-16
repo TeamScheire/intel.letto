b52610e9ab3e9c9e5990aa0b454b5870bb376f46
Maria-Cristina Ciocci <mcristina.ciocci@gmail.com>
2018-02-16 18:10:02 +0100

    slave master control of the massage mattress

---
+++ alarmblanket/ArduinoTestSketches/massage_control_mqtt_MASTER_NODEMCU/massage_control_mqtt_MASTER_NODEMCU.ino
@@ -0,0 +1,105 @@
+// Ingegno 02/2017
+// control massage mattress
+// Wiring:
+// 12V input to Arduino
+// 5V out to drive NodeMCU
+// MASTER: This is code on the NodeMCUArduino, which is the master device.
+//         Based on mqtt request we set massage on the device. 
+
+/*  START USER SETTABLE OPTIONS */
+bool use_static_IP = false;         //use a static IP address
+uint8_t static_IP[4] = {192, 168, 1, 42}; 
+uint8_t static_gateway_IP[4] = {192, 168, 1, 1};// if you want to use static IP make sure you know what the gateway IP is, here 192.168.1.1
+
+// wifi data
+// write here your wifi credentials 
+const char* ssid = "*****";   // insert your own ssid 
+const char* password = "********"; // and password
+
+//mqtt server/broker 
+//const char* mqtt_server = "broker.mqtt-dashboard.com";
+const char* mqtt_server = "192.168.0.111";
+
+/*  END USER SETTABLE OPTIONS */
+
+// I2C on D2 = SDA and D1 =SCL 
+int pSDA = D2;
+int pSCL = D3;
+
+//massage mattress controller
+const byte SLAVE_ADDRESS = 9;  // Massage control slave device
+
+enum MASSAGE {MS_NONE, 
+              MS_NECKON, MS_NECKOFF, MS_NECK, MS_NECKWEAK,
+              MS_BREASTON, MS_BREASTOFF, MS_BREAST, MS_BREASTWEAK, 
+              MS_BELLYON, MS_BELLYOFF, MS_BELLY, MS_BELLYWEAK, 
+              MS_HIPON, MS_HIPOFF, MS_HIP, MS_HIPWEAK, 
+              MS_F1};
+               
+MASSAGE massagescenario = MS_NONE;
+
+//includes
+#include <Wire.h>
+
+// debug info
+#define SERIALTESTOUTPUT false
+
+void send2Slave(int msg) {
+  Wire.beginTransmission(SLAVE_ADDRESS); // transmit to device SLAVE_ADDRESS
+  Wire.write(msg);           // sends one byte
+  Wire.endTransmission();    // stop transmitting
+}
+
+//wifi and timing lib
+#include "wifilib.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  if (SERIALTESTOUTPUT) Serial.begin(115200); // most ESP-01's use 115200 but this could vary
+
+  //set up I2C bus 
+  Wire.begin(pSDA, pSCL);   // Initialiseer de I2C bus op pinnen GPIO0 (= D3) SDA en GPIO 02 (= D4) SCL
+
+  while (WiFi.status() != WL_CONNECTED) {
+   setupWiFi(true); // Connect to local Wifi
+  }
+  //set randomseed
+  randomSeed(micros());
+
+  // mqtt client start
+  // Here we subscribe to MQTT topic intellettoMassage
+  setupMQTTClient();
+
+  if (SERIALTESTOUTPUT) {
+    Serial.println("");
+    Serial.print("Connected to WiFi at ");
+  }
+  ip = WiFi.localIP();
+  if (SERIALTESTOUTPUT) {
+    Serial.print(ip);
+    Serial.println("");
+    Serial.print("Wifi end of IP: ");
+    Serial.print(ip[3]);
+    Serial.println("");
+  }
+  obtainedwifi = true;
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //handle MQTT messages
+  handleMQTTClient();
+
+  // if disconnected, reconnect to wifi:
+  if ( WiFi.status() != WL_CONNECTED) {
+      delay(1);
+      setupWiFi(false);
+  }
+  
+  if (SERIALTESTOUTPUT) {
+    delay(500);
+  } 
+}
+
+
---
+++ alarmblanket/ArduinoTestSketches/massage_control_mqtt_SLAVE_ARD/massage_control_mqtt_SLAVE_ARD.ino
@@ -0,0 +1,252 @@
+// Ingegno 02/2017
+// control massage mattress
+// Wiring:
+// 12V input to Arduino
+// pin 3 to circuit to control motor 1 
+// SLAVE: This is code on the Arduino, which is a slave device
+//        of a NodeMCU via I2C. 
+
+#include <Wire.h>
+
+bool USE_I2C = true;
+bool USE_SERIAL = ! USE_I2C;
+int intervalserial = 1000;   //output to serial
+unsigned long lastMsg;
+
+// auto switch off after maxMasDuration (15 min)
+unsigned long maxMasDuration = 15 * 60 *1000L;;
+
+const byte SLAVE_ADDRESS = 9;  // Massage control slave device
+
+//pin to the different motors
+int NeckMotorsSTRONG = 2;
+int NeckMotorsWEAK = 3;
+int BreastMotorsSTRONG = 4;
+int BreastMotorsWEAK = 5;
+int BellyMotorsSTRONG = 6;
+int BellyMotorsWEAK = 7;
+int HipMotorsSTRONG = 8;
+int HipMotorsWEAK = 9;
+
+int curMotor = 0;
+int maxMotor = 7;
+int Motors[8] = {NeckMotorsSTRONG, NeckMotorsWEAK, BreastMotorsSTRONG,
+                BreastMotorsWEAK, BellyMotorsSTRONG, BellyMotorsWEAK,
+                HipMotorsSTRONG, HipMotorsWEAK};
+
+enum MASSAGE {MS_NONE, 
+              MS_NECKON, MS_NECKOFF, MS_NECK, MS_NECKWEAK,
+              MS_BREASTON, MS_BREASTOFF, MS_BREAST, MS_BREASTWEAK, 
+              MS_BELLYON, MS_BELLYOFF, MS_BELLY, MS_BELLYWEAK, 
+              MS_HIPON, MS_HIPOFF, MS_HIP, MS_HIPWEAK, 
+              MS_F1};
+              
+
+MASSAGE massagescenario = MS_NONE;
+bool massagechanged = false;
+
+void MotorsOff() {
+   digitalWrite(NeckMotorsSTRONG, LOW);
+   digitalWrite(NeckMotorsWEAK, LOW);
+   digitalWrite(BreastMotorsSTRONG, LOW);
+   digitalWrite(BreastMotorsWEAK, LOW);
+   digitalWrite(BellyMotorsSTRONG, LOW);
+   digitalWrite(BellyMotorsWEAK, LOW);
+   digitalWrite(HipMotorsSTRONG, LOW);
+   digitalWrite(HipMotorsWEAK, LOW);
+}
+
+void Off() {
+  MotorsOff();
+}
+void setup() {
+  pinMode(NeckMotorsSTRONG, OUTPUT);   // sets the pin as output
+  pinMode(BreastMotorsSTRONG, OUTPUT);   // sets the pin as output
+  pinMode(BellyMotorsSTRONG, OUTPUT);   // sets the pin as output
+  pinMode(HipMotorsSTRONG, OUTPUT);   // sets the pin as output
+  pinMode(NeckMotorsWEAK, OUTPUT);   // sets the pin as output
+  pinMode(BreastMotorsWEAK, OUTPUT);   // sets the pin as output
+  pinMode(BellyMotorsWEAK, OUTPUT);   // sets the pin as output
+  pinMode(HipMotorsWEAK, OUTPUT);   // sets the pin as output
+  
+  if (USE_SERIAL) {
+    //Serial.begin(115200);
+    Serial.begin(57600);                    // connect to the serial port
+    Serial.println("Matrass Controller");
+  }
+  if (USE_I2C) {
+    Wire.begin(SLAVE_ADDRESS);
+    Wire.onReceive(receiveEvent); // register event
+  }
+  
+}
+
+void loop() {
+  unsigned long now = millis(); 
+  if (USE_SERIAL) {                
+    if (now - lastMsg > intervalserial) {  // print every "interval" milliseconds or longer
+      lastMsg = now;
+      Serial.print("Doing Motor "); Serial.println(Motors[curMotor]);
+      MotorsOff();
+      digitalWrite(Motors[curMotor], HIGH);
+      curMotor += 1;
+      if (curMotor > maxMotor) {curMotor = 0;}
+    }
+  } else {
+    // I2C SLAVE Device operation
+    if (massagescenario == MS_NONE) {
+      if (massagechanged) {
+       Off();
+       massagechanged = false;
+      }
+      //wait for input
+      delay(200);
+    } else if (now - lastMsg > maxMasDuration) {
+      //switch off
+      massagescenario == MS_NONE;
+      massagechanged = true;
+    } else {
+      handleMassageScenario();
+    }
+  }
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+// we receive a single integer which indicates what massage to do.
+void receiveEvent(int howMany) {
+  lastMsg = millis();
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+  }
+  int x = Wire.read();    // receive byte as an integer
+
+  massagescenario = (MASSAGE) x;
+  massagechanged = true;
+}
+
+void handleMassageScenario() {
+  // there is one of the scenario's, we execute on it.
+  if (massagescenario == MS_NECK) {
+    if (massagechanged) {
+      MotorsOff();
+      //switch on NECK motor
+      digitalWrite(NeckMotorsSTRONG, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_NECKWEAK) {
+    if (massagechanged) {
+      MotorsOff();
+      //switch on NECK motor
+      digitalWrite(NeckMotorsWEAK, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_BREAST) {
+    if (massagechanged) {
+      MotorsOff();
+      //switch on motor
+      digitalWrite(BreastMotorsSTRONG, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_BREASTWEAK) {
+    if (massagechanged) {
+      MotorsOff();
+      //switch on motor
+      digitalWrite(BreastMotorsWEAK, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_BELLY) {
+    if (massagechanged) {
+      MotorsOff();
+      //switch on motor
+      digitalWrite(BellyMotorsSTRONG, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_BELLYWEAK) {
+    if (massagechanged) {
+      MotorsOff();
+      //switch on motor
+      digitalWrite(BellyMotorsWEAK, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_HIP) {
+    if (massagechanged) {
+      MotorsOff();
+      //switch on motor
+      digitalWrite(HipMotorsSTRONG, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_HIPWEAK) {
+    if (massagechanged) {
+      MotorsOff();
+      //switch on motor
+      digitalWrite(HipMotorsWEAK, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_NECKOFF) {
+    if (massagechanged) {
+      //switch off motor
+      digitalWrite(NeckMotorsSTRONG, LOW);
+      digitalWrite(NeckMotorsWEAK, LOW);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_BREASTOFF) {
+    if (massagechanged) {
+      //switch off motor
+      digitalWrite(BreastMotorsSTRONG, LOW);
+      digitalWrite(BreastMotorsWEAK, LOW);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_BELLYOFF) {
+    if (massagechanged) {
+      //switch off motor
+      digitalWrite(BellyMotorsSTRONG, LOW);
+      digitalWrite(BellyMotorsWEAK, LOW);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_HIPOFF) {
+    if (massagechanged) {
+      //switch off motor
+      digitalWrite(HipMotorsSTRONG, LOW);
+      digitalWrite(HipMotorsWEAK, LOW);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_NECKON) {
+    if (massagechanged) {
+      //switch off motor
+      digitalWrite(NeckMotorsWEAK, LOW);
+      digitalWrite(NeckMotorsSTRONG, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_BREASTON) {
+    if (massagechanged) {
+      //switch off motor
+      digitalWrite(BreastMotorsWEAK, LOW);
+      digitalWrite(BreastMotorsSTRONG, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_BELLYON) {
+    if (massagechanged) {
+      //switch off motor
+      digitalWrite(BellyMotorsWEAK, LOW);
+      digitalWrite(BellyMotorsSTRONG, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_HIPON) {
+    if (massagechanged) {
+      //switch off motor
+      digitalWrite(HipMotorsWEAK, LOW);
+      digitalWrite(HipMotorsSTRONG, HIGH);
+      massagechanged = false;
+    }
+  } else if (massagescenario == MS_F1) {
+    if (massagechanged) {
+      MotorsOff();
+      //switch on motor
+      massagechanged = false;
+    }
+    // F1 is an effect! Depending on the state motors are on/off
+  }
+}
+
+
